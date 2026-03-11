169a23fd70fa84575143a9b18ee01c014804f9da
CWHengProj <142023227+CWHengProj@users.noreply.github.com.>
2026-03-11 09:50:45 +0800

    Added clear feature and faux loading animation placeholder - not working currently

--- main.go
+++ main.go
@@ -3,22 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
+	"time"
 )
 
 func main() {
 	var i string
 	fmt.Println("quack - enter response here")
 	for true {
-		//TODO: start cli application here
 		fmt.Scan(&i)
-		//TODO: add support to check for "exit"
+		//TODO: concurrent checks for multiple things? graceful exit?
 		//TODO: shift + enter for new line
 		if strings.Split(i, " ")[0] == "exit" {
-			fmt.Print("quack - gracefully exiting..")
+			fmt.Println("quack - gracefully exiting..")
 			os.Exit(0)
 		}
-		//TODO: add option to clear the chat
+		if strings.Split(i, " ")[0] == "clear" {
+			fmt.Println("clearing chat")
+			clearScreen
+			fmt.Printf("\r"
+			//TODO: add option to clear the chat
+			os.Exit(0)
+		}
+		// Faux loading
+		// TODO: using ANSI for polish phase
+		for i := 0; i <= 100; i++ {
+			fmt.Printf("\rLoading... %d%%", i)
+			time.Sleep(50 * time.Millisecond)
+		}
+
 		//TODO: add option to retain the chat history in logs
 		fmt.Println(getRandomResponse())
 	}
@@ -33,3 +47,8 @@ func init() {
 	//TODO: add init ascii here
 }
 
+var clearScreen = func() {
+    cmd := exec.Command(`printf '\33c\e[3J'`) // clears the scrollback buffer as well as the screen.
+    cmd.Stdout = os.Stdout
+    cmd.Run()
+}
